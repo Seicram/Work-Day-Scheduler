3702bc0dc51e79085ddcdb1d39d3c225ff57dbf1
marcies smith <marcies.smith@gmail.com>
2023-05-16 13:23:16 -0400

    completed the js code for push

--- Develop/script.js
+++ Develop/script.js
@@ -1,36 +1,34 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
-  
   var Target = document.getElementById('currentDay');
-  function clock () {
-    var time = new date ();
 
-    var month = time.month();
-    var date = time.date();
-    var day = time.day();
-    var week = time.week['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    var year = time.year['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  function clock() {
+    var time = new Date();
 
-    var hours = time.hours();
-    var minutes = time.minutes();
-    
-    Target.innerText = 
-    `${year[month]}. ${date}. ${week[day]}. ` +
-    `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
+    var month = time.getMonth();
+    var date = time.getDate();
+    var day = time.getDay();
+    var week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    var year = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
+    var hours = time.getHours();
+    var minutes = time.getMinutes();
+    var seconds = time.getSeconds();
+
+    Target.innerText =
+      `${year[month]} ${date}, ${week[day]}` +
+      ` ${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
   }
+
   clock();
   setInterval(clock, 1000);
+
   $(".saveBtn").on("click", function () {
-    console.log(this);
     var scheduledEvent = $(this).siblings(".description").val();
     var scheduledTime = $(this).parent().attr("id");
 
     localStorage.setItem(scheduledTime, scheduledEvent);
+  });
 
-  })
   $("#hour-9 .description").val(localStorage.getItem("hour-9"));
   $("#hour-10 .description").val(localStorage.getItem("hour-10"));
   $("#hour-11 .description").val(localStorage.getItem("hour-11"));
@@ -40,11 +38,10 @@ $(function () {
   $("#hour-15 .description").val(localStorage.getItem("hour-15"));
   $("#hour-16 .description").val(localStorage.getItem("hour-16"));
   $("#hour-17 .description").val(localStorage.getItem("hour-17"));
-  const dayjs = require("dayjs");
-  
+
   function color() {
     var currentTime = dayjs().get('h');
-  
+
     var hour9 = 9;
     var hour10 = 10;
     var hour11 = 11;
@@ -55,26 +52,9 @@ $(function () {
     var hour16 = 16;
     var hour17 = 17;
 
-   
+    // Add your logic to apply appropriate styling based on the current time and the time blocks here
 
   }
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  color();
 });
